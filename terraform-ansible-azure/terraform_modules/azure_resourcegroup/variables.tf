b63d8fe8c6bdabeba592716b812855d37884ceee
Phong Vu <pvu@ubu-automation.T-NetPro.de>
2020-11-03 21:18:06 +0100

    syntax corrections

--- terraform-ansible-azure/terraform_modules/azure_resourcegroup/variables.tf
+++ terraform-ansible-azure/terraform_modules/azure_resourcegroup/variables.tf
@@ -20,8 +20,7 @@ variable "client_secret" {
 }
 
 variable "tenant_id" {
-  description = "Enter Tenant ID / Directory ID of your Azure AD. Run Get-AzureSubscription to know your Tenant
-ID"
+  description = "Enter Tenant ID / Directory ID of your Azure AD. Run Get-AzureSubscription to know your Tenant ID"
 }
 
 variable "location" {
